2552fa7a86271ab3ee85673c134ea241a99e7e76
Quynh Nguyen <qnguyenx3@gmail.com>
2017-07-26 18:06:54 -0700

    Add method_detective.rb

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,42 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+# "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+# "zom".replace "zoom"
 # => “zoom”
 
-# "enhance".<???>
+# "zom".insert (1, "o")
+# => “zoom”
+
+# "enhance".center(20)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+# "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+# "the usual" << " suspects"
 #=> "the usual suspects"
 
-# " suspects".<???>
+# " suspects".prepend "the usual"
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+# "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+# "The mystery of the missing first letter".gsub(/^./,"")
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+# "Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+# "z".each_codepoint.to_a
+# => 122
 
-# "How many times does the letter 'a' appear in this string?".<???>
+# "z".ord
+# => 122
+# (What is the significance of the number 122 in relation to the character z?) The number 122 is an integer ordinal of the letter z.
+
+# "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
