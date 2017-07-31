a700604278ebec7c4959ad1f96548c45ba25fe6c
Quynh Nguyen <qnguyenx3@gmail.com>
2017-07-31 16:55:16 -0700

    Add puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# PUPPY ------------------------
+# Corgi
+
+# CHARACTERISTICS ----------------
+# Eyelash length: varies
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: brown and white
+
+# BEHAVIORS -----------------------
+# Jump
+# Bark
+# Lick
+# Hug
+# Catch
