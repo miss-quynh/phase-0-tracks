c081026a4c03ca566f9566351dfe3cb59b673fce
Quynh Nguyen <qnguyenx3@gmail.com>
2017-08-18 21:27:50 -0700

    Add my_solution.rb

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,29 +1,34 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge by myself.
+# I spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# require_relative complements the built-in method require by allowing you to load a file that is relative to the file containing the require_relative statement.
+
 require_relative 'state_data'
 
 class VirusPredictor
 
+# The initialize method assigns default values into all the instance variables
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+# The virus_effects method defines two types of arguments that result from the virus spread
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    number_of_deaths = predicted_deaths()
+    speed = speed_of_spread()
+    return "#{@state} will lose #{number_of_deaths} people from the outbreak, and the virus will spread in #{speed} months."
   end
 
+# The private method cannot be called with an explicit receiver as the receiver is always self. This means that the private method can be called only in the context of the current object; therefore, when I try to call predicted_deaths and speed_of_spread methods outside the current object, I will get an access violation runtime error.
   private
 
-  def predicted_deaths(population_density, population, state)
+# The predicted_deaths method provides the number of deaths based on the population density in each state
+  def predicted_deaths()
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -37,11 +42,12 @@ class VirusPredictor
       number_of_deaths = (@population * 0.05).floor
     end
 
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+    return number_of_deaths
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+#The speed_of_spread method determines how rapidly the outbreak will spread based on the population density in each state.
+  def speed_of_spread() #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -58,7 +64,7 @@ class VirusPredictor
       speed += 2.5
     end
 
-    puts " and will spread across the state in #{speed} months.\n\n"
+    return speed
 
   end
 
@@ -69,19 +75,25 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
+# The report ran for all 50 states can be either in the class or outside the class. It can be in the class if you want to create another method to call upon. However, in my case, I added the driver code outside the class to run the report on all 50 states.
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
-
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
-
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
-
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+STATE_DATA.each do |state_name, population_info|
+  state = VirusPredictor.new(state_name, population_info[:population_density], population_info[:population])
+  print state.virus_effects
+  puts ""
+end
 
 
 #=======================================================================
 # Reflection Section
+
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+  # The overall STATE_DATA hash contains strings keys and the sub hash of each state contains symbol keys. The string keys use the hash rocket to link the key in value, and the symbol keys use the colon to define the value.
+# What does require_relative do? How is it different from require?
+  # require_relative complements the built-in method require by allowing you to load a file that is relative to the file containing the require_relative statement.
+# What are some ways to iterate through a hash?
+  # A hash can be iterated using .each or .map keys
+# When refactoring virus_effects, what stood out to you about the variables, if anything?
+  # The global variables were not needed to be passed through as parameters when defining the method
+# What concept did you most solidify in this challenge?
+  # How to iterate through a hash and learned how to differentiate between block parameters and local and global variables
